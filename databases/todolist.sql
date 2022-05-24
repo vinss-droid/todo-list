5045b092d2c00e3a574e284ea4444bf9622f0ce9
Kevin Sipahutar <kevinsipahutar220604@gmail.com>
2022-05-24 23:27:43 +0700

    Todo List

---
+++ databases/todolist.sql
@@ -0,0 +1,87 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Waktu pembuatan: 24 Bulan Mei 2022 pada 15.59
+-- Versi server: 5.7.33
+-- Versi PHP: 7.4.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `todolist`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `todos`
+--
+
+CREATE TABLE `todos` (
+  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `judul` varchar(255) NOT NULL,
+  `tgl` varchar(255) NOT NULL,
+  `prioritas` enum('tinggi','rendah','sedang') NOT NULL,
+  `selesai` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `todos`
+--
+ALTER TABLE `todos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `todos`
+--
+ALTER TABLE `todos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ index.php
@@ -0,0 +1,221 @@
+<?php
+
+error_reporting(E_ALL);
+
+include './koneksi.php';
+
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("location:login.php");
+}
+
+if (isset($_POST['simpan'])) {
+    $id_user = $_SESSION['id'];
+    $judul = $_POST['judul'];
+    $tgl = date('d F Y', strtotime($_POST['tanggal']));
+    $prioritas = $_POST['prioritas'];
+
+    // var_dump($prioritas);
+
+    $sql = mysqli_query($conn, "INSERT INTO todos VALUES (NULL,'$id_user','$judul', '$tgl', '$prioritas', '0')");
+
+    if (!$sql) {
+        echo "
+            <script>
+                alert('Gagal Menambahkan Todo List');
+                window.location.href = './index.php';
+            </script>
+       ";
+    } else {
+
+        header("location:index.php");
+
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TODO LIST</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./asset/css/style.css">
+</head>
+
+<body class="bg-black text-white">
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <h1 class="text-center fw-bold mt-5">TO DO LIST</h4>
+
+                <div class="col-sm-12 col-md-12 col-lg-10">
+                    <div class="card shadow-lg bg-dark">
+                        <div class="card-body">
+                            <div class="table-responsive">
+                            
+                                <div class="container">
+                                    <button type="button" class="btn btn-primary btn-sm my-3" id="tambah">Tambah</button>
+                                    <button class="btn btn-sm btn-success" id="riwayat">Todo List Selesai</button>
+                                    <button class="btn btn-sm btn-info d-none" id="sekarang">Todo List Sekarang</button>
+                                    <a href="./logout.php" class="btn btn-danger btn-sm my-3">Keluar</a>
+                                </div>
+
+                                <table class="table table-hover text-center text-white table-dark">
+                                    <thead>
+                                        <tr>
+                                            <th class="col-1">No</th>
+                                            <th class="col-4">Judul</th>
+                                            <th class="col-3">Tanggal</th>
+                                            <th class="col-2">Prioritas</th>
+                                            <th class="col-2">Aksi</th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody id="listSekarang">
+
+                                    <?php
+
+                                    $no = 1;
+
+                                    $id_user = $_SESSION['id'];
+                                    
+                                    $query = mysqli_query($conn, "SELECT * FROM todos WHERE id_user = '$id_user' AND selesai = '0' ORDER BY tgl ASC, prioritas");
+
+                                    while ($data = mysqli_fetch_assoc($query)) {
+
+                                    ?>
+
+                                            <tr>
+                                                <td class="col-1"><?= $no++ ?></td>
+                                                <td class="col-4"><?= $data['judul']; ?></td>
+                                                <td class="col-3"><?= $data['tgl']; ?></td>
+                                                <td class="col-2"><?= ucwords($data['prioritas']) ?></td>
+                                                <td class="col-2">
+                                                    <a href="./selesai.php?id=<?= $data['id'] ?>" class="btn btn-sm btn-outline-success">Selesai</a>
+                                                </td>
+                                            </tr>
+
+                                    <?php } ?>
+
+                                    </tbody>
+
+                                    <tbody class="d-none" id="listRiwayat">
+
+                                        <?php
+
+                                        $no = 1;
+
+                                        $id_user = $_SESSION['id'];
+                                        
+                                        $query = mysqli_query($conn, "SELECT * FROM todos WHERE id_user = '$id_user' AND selesai = '1' ORDER BY tgl ASC, prioritas");
+
+                                        while ($data = mysqli_fetch_assoc($query)) {
+
+                                        ?>
+
+                                                <tr>
+                                                    <td class="col-1"><?= $no++ ?></td>
+                                                    <td class="col-4"><?= $data['judul']; ?></td>
+                                                    <td class="col-3"><?= $data['tgl']; ?></td>
+                                                    <td class="col-2"><?= ucwords($data['prioritas']) ?></td>
+                                                    <td class="col-2">
+                                                        <button type="button" class="btn btn-sm btn-success" disabled>Selesai</button>
+                                                    </td>
+                                                </tr>
+
+                                        <?php } ?>
+
+                                    </tbody>
+
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+        </div>
+    </div>
+
+    <!-- Modal Register -->
+    <div class="modal fade" id="Mtambah" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content bg-dark">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Tambah Todo List</h5>
+                    <button type="button" class="btn-close bg-white" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="container">
+                        <form action="" method="post">
+                            <div class="mb-4">
+                                <label for="judul" class="form-label">Judul</label>
+                                <input type="text" class="form-control" placeholder="Masukkan Judul" required id="judul" name="judul">
+                            </div>
+                            <div class="mb-4">
+                                <label for="tanggal" class="form-label">Tanggal</label>
+                                <input type="date" class="form-control" placeholder="Masukkan Tanggal" required id="tanggal" name="tanggal">
+                            </div>
+                            <div class="mb-4">
+                                <label for="prioritas" class="form-label">Prioritas</label>
+                                <select name="prioritas" id="prioritas" class="form-select text-center" required>
+                                    <option value="" selected class="text-center">----- Pilih Prioritas -----</option>
+                                    <option value="tinggi" class="text-center">Tinggi</option>
+                                    <option value="sedang" class="text-center">Sedang</option>
+                                    <option value="rendah" class="text-center">Rendah</option>
+                                </select>
+                            </div>
+                            <div class="d-grid">
+                                <button class="btn btn-primary" name="simpan">Simpan</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <!-- <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Understood</button>
+      </div> -->
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.slim.js" integrity="sha256-HwWONEZrpuoh951cQD1ov2HUK5zA5DwJ1DNUXaM6FsY=" crossorigin="anonymous"></script>
+
+    <script>
+        $(document).ready(function() {
+
+            $('#tambah').click(function() {
+                $('#Mtambah').modal('show');
+            })
+
+            $('#riwayat').click(function (e) { 
+                e.preventDefault();
+                
+                $(this).addClass('d-none');
+                $('#sekarang').removeClass('d-none');
+                $('#listSekarang').addClass('d-none');
+                $('#listRiwayat').removeClass('d-none');
+
+            });
+            
+            $('#sekarang').click(function (e) { 
+                e.preventDefault();
+                
+                $(this).addClass('d-none');
+                $('#riwayat').removeClass('d-none');
+                $('#listRiwayat').addClass('d-none');
+                $('#listSekarang').removeClass('d-none');
+
+            });
+        })
+    </script>
+
+</body>
+
+</html>
